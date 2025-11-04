b18845a1e192817fa45c192ba5fc9b7493f747d5
Julia Cezar <cj51264>
2025-11-04 21:48:41 +0100

    labjakis

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -18,6 +18,7 @@ use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 final class ForecastController extends AbstractController
 {
     #[Route(name: 'app_forecast_index', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_INDEX')]
     public function index(ForecastRepository $forecastRepository): Response
     {
         return $this->render('forecast/index.html.twig', [
@@ -39,6 +40,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/new', name: 'app_forecast_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $forecast = new Forecast();
@@ -61,6 +63,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_forecast_show', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_SHOW')]
     public function show(Forecast $forecast): Response
     {
         return $this->render('forecast/show.html.twig', [
@@ -69,6 +72,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_forecast_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_EDIT')]
     public function edit(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ForecastType::class, $forecast, [
@@ -89,6 +93,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_forecast_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_FORECAST_DELETE')]
     public function delete(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$forecast->getId(), $request->getPayload()->getString('_token'))) {
